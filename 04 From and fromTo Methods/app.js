3db9c18b8192eb8c883d6436e9f81873d0d43f1b
Sankaranarayanan <sankar241101@gmail.com>
2024-10-19 21:47:18 +0530

    Project 4

---
+++ 04 From and fromTo Methods/app.js
@@ -0,0 +1,28 @@
+///form in from method you don't have to specify the intial state.(it will figure out by itself)
+
+
+// gsap.from(".box",   {
+//     y:-200,
+//     duration:3,
+//     ease:"liner",
+//     repeat:-1,
+//     yoyo:true,
+// })
+
+
+//fromTo in formTo method  you Have to specify defult value, and also the new animation state.
+
+
+gsap.fromTo(
+    ".box",
+    { y: 200, opacity: 0 },
+    {
+      opacity: 1,
+      y: -200,
+      duration: 3,
+      ease: "linear",
+      borderRadius: 0,
+      repeat: -1,
+      yoyo: true,
+    }
+  );
